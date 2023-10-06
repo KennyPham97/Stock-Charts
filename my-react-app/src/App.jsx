d9ed75e9449030b757b0b62868efd8fb670ac9a0
COR Coding <corcoding@Coding-1.attlocal.net>
2023-10-06 00:25:38 -0400

    added charts to every ticker, styled the page with better fonts

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Spy from './components/Spy';
 import Apple from './components/Apple';
 import Amazon from './components/Amazon';
 import Tesla from './components/Tesla';
@@ -8,24 +9,29 @@ import Alphabet from './components/Alphabet';
 import Netflix from './components/Netflix';
 import Facebook from './components/Facebook';
 import StockSearch from './components/StockSearch';
-import StockiPro from './components/StockiPro';
+import StockSurferPro from './components/StockSurferPro';
+import chartIcon from './images/chart-icon.png'; 
 import './App.css';
 
 const App = () => {
-  const [showStockiPro, setShowStockiPro] = useState(false);
+  const [showStockSurferPro, setShowStockSurferPro] = useState(false);
 
-  const handleGetStockiProClick = () => {
-    setShowStockiPro(true);
+  const handleGetStockSurferProClick = () => {
+    setShowStockSurferPro(true);
   };
 
   return (
     <div className="app-container">
       <div>
-      <h1 className='header'>STOCKI</h1>
-        <h3>Welcome to STOCKI, where you can get daily data on your favorite stocks.</h3>
+      <img className ='chart-icon'src={chartIcon} alt="Chart Icon"></img>
+      <h1 className='header'>StockSurfer</h1>
+      
+        <h3>Welcome to StockSurfer, where you can get data on your favorite stocks.</h3>
         <h3>Choose from some popular stocks here:</h3>
         <NavBar />
         <Routes>
+          <Route path="/" element={<Spy />} />
+          <Route path="/spy" element={<Spy />} />
           <Route path="/apple" element={<Apple />} />
           <Route path="/facebook" element={<Facebook />} />
           <Route path="/amazon" element={<Amazon />} />
@@ -34,9 +40,9 @@ const App = () => {
           <Route path="/tesla" element={<Tesla />} />
           <Route path="/search" element={<StockSearch />} />
         </Routes>
-        <h3>Get Stocki Pro to search any ticker and more advanced features</h3>
-        <button onClick={handleGetStockiProClick}>Get Stocki Pro</button>
-        {showStockiPro && <StockiPro />}
+        <h3>Get StockSurfer Pro to search any ticker and more advanced features</h3>
+        <button onClick={handleGetStockSurferProClick}>Get StockSurfer Pro</button>
+        {showStockSurferPro && <StockSurferPro />}
       </div>
     </div>
   );
--- my-react-app/src/components/Facebook.jsx
+++ my-react-app/src/components/Facebook.jsx
@@ -1,10 +1,79 @@
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+
+// const Facebook = () => {
+//   const [stockData, setStockData] = useState({});
+//   const [companyInfo, setCompanyInfo] = useState({});
+
+//   useEffect(() => {
+//     axios
+//       .get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=META&apikey=TEPTNV3NRFJ3ZKWQ`)
+//       .then((response) => {
+//         console.log('Stock Data Response:', response.data);
+//         setStockData(response.data['Time Series (Daily)']);
+//       });
+//     axios
+//       .get(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=META&apikey=TEPTNV3NRFJ3ZKWQ`)
+//       .then((response) => {
+//         console.log('Company Info Response:', response.data);
+//         setCompanyInfo(response.data);
+//       });
+//   }, []);
+
+//   return (
+//     <div>
+//       <h2>FaceBook Stock Data</h2>
+//       <div>
+//         <h3>Company Information</h3>
+//         <ul>
+//           <p>Symbol: {companyInfo.Symbol}</p>
+//           <p>Name: {companyInfo.Name}</p>
+//           <p>Industry: {companyInfo.Industry}</p>
+//           <p>Sector: {companyInfo.Sector}</p>
+//           <p>Market Cap: {companyInfo.MarketCapitalization}</p>
+//           <p>Description: {companyInfo.Description}</p>
+          
+//         </ul>
+//       </div>
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>Date</th>
+//             <th>Open</th>
+//             <th>High</th>
+//             <th>Low</th>
+//             <th>Close</th>
+//             <th>Volume</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {Object.keys(stockData).map((date) => (
+//             <tr key={date}>
+//               <td>{date}</td>
+//               <td>{stockData[date]['1. open']}</td>
+//               <td>{stockData[date]['2. high']}</td>
+//               <td>{stockData[date]['3. low']}</td>
+//               <td>{stockData[date]['4. close']}</td>
+//               <td>{stockData[date]['5. volume']}</td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
+
+// export default Facebook
+
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Line } from 'react-chartjs-2';
+import Plot from 'react-plotly.js';
 
 const Facebook = () => {
   const [stockData, setStockData] = useState({});
   const [companyInfo, setCompanyInfo] = useState({});
+  const [dateList, setDateList] = useState([]);
+  const [closeList, setCloseList] = useState([]);
 
   useEffect(() => {
     axios
@@ -12,6 +81,18 @@ const Facebook = () => {
       .then((response) => {
         console.log('Stock Data Response:', response.data);
         setStockData(response.data['Time Series (Daily)']);
+
+        const dates = [];
+        const closes = [];
+
+        for (const date in response.data['Time Series (Daily)']) {
+          const closeValue = response.data['Time Series (Daily)'][date]['4. close'];
+          dates.unshift(date); // Reverse order to show the latest date first
+          closes.unshift(parseFloat(closeValue));
+        }
+
+        setDateList(dates);
+        setCloseList(closes);
       });
     axios
       .get(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=META&apikey=TEPTNV3NRFJ3ZKWQ`)
@@ -21,45 +102,39 @@ const Facebook = () => {
       });
   }, []);
 
+  const chartData = [
+    {
+      x: dateList,
+      y: closeList,
+      type: 'scatter',
+      mode: 'lines',
+      marker: { color: 'black' },
+    },
+  ];
+
+  const chartLayout = {
+    title: 'Facebook Stock Daily Price',
+    xaxis: { title: 'Date' },
+    yaxis: { title: 'Closing Price' },
+  };
+
   return (
     <div>
-      <h2>FaceBook Stock Data</h2>
+      <h2>Facebook Stock Data</h2>
       <div>
         <h3>Company Information</h3>
         <ul>
-          <li>Symbol: {companyInfo.Symbol}</li>
-          <li>Name: {companyInfo.Name}</li>
-          <li>Industry: {companyInfo.Industry}</li>
-          <li>Market Cap: {companyInfo.MarketCapitalization}</li>
-          
+          <p>Symbol: {companyInfo.Symbol}</p>
+          <p>Name: {companyInfo.Name}</p>
+          <p>Industry: {companyInfo.Industry}</p>
+          <p>Sector: {companyInfo.Sector}</p>
+          <p>Market Cap: {companyInfo.MarketCapitalization}</p>
+          <p>Description: {companyInfo.Description}</p>
         </ul>
       </div>
-      <table>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Open</th>
-            <th>High</th>
-            <th>Low</th>
-            <th>Close</th>
-            <th>Volume</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.keys(stockData).map((date) => (
-            <tr key={date}>
-              <td>{date}</td>
-              <td>{stockData[date]['1. open']}</td>
-              <td>{stockData[date]['2. high']}</td>
-              <td>{stockData[date]['3. low']}</td>
-              <td>{stockData[date]['4. close']}</td>
-              <td>{stockData[date]['5. volume']}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Plot data={chartData} layout={chartLayout} />
     </div>
   );
 };
 
-export default Facebook
+export default Facebook;
